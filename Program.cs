55d76a27aa79c4711e5cc4c22815d5d5131455e2
YusufPutraS <yusufputra274@gmail.com>
2021-03-13 22:15:44 +0700

    project pertama

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (operand == "1")
+                Console.Write("Bilangan 1:");
+            int a = int.Parse(Console.ReadLjne())
+            {
+                Console.Write(penambahan(a, b));
+            }
+            else if (operand == "2")
+                Console.Write("Bilangan 2:");
+            int a = int.Parse(Console.ReadLjne())
+            {
+                Console.Write(pengurangan(a, b));
+            }
+            else if (operand == "3")
+                Console.Write("Bilangan 3:");
+            int a = int.Parse(Console.ReadLjne())
+            {
+                Console.Write(pembagian(a, b));
+            }
+            else if (operand == "4")
+                Console.Write("Bilangan 4:");
+            int a = int.Parse(Console.ReadLjne())
+            {
+                Console.Write(pengalian(a, b));
+            }
+        }
+
+
+                    static int penambahan(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int pengurangan(int a, int b);
+        {
+            return a - b;
+        }
+
+        static int pembagian(int a, int b);
+        {
+            return a / b;
+        }
+
+        static int perkalian(int a, int b);
+        {
+            return a * b;
+        }
+}
+
+Console.ReadLine();
+        }
+
+Console.Title = "Aplikasi Kalkulator";
+
+            int a = 10; // declarasi variabel a,dg  nilai awal 10;
+            int a = 6; // declarasi variabel b,dg  nilai awal 6;)
+
+            Console.WriteLine(Hasil Penambahan " + a + " + " + b + " = +Penambahan(a, b));
+            Console.Write("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            Console.Write("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+            Console.Write("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            console.ReadKey(
+
+            ExitProgram();
+        }
+
+
+    }
+// Credit : Yusuf Putra Santoso [20.11.3337]
